04f123bfdf4d9fd426f1680be1c55d857b0b1a03
Logan Allred <allredlt@familysearch.org>
2012-09-09 17:41:29 -0600

    Refactor authorization from everyauth to passport.
    Cleaned up the example app as well, and added some debug logging, and updated documentation.

--- README.md
+++ README.md
@@ -5,28 +5,41 @@ Asynchronous FamilySearch client API for Node.js
 
 [![Build Status](https://secure.travis-ci.org/redbugz/node-fsapi.png)](http://travis-ci.org/redbugz/node-fsapi)
 
-## Version 0.0.1
+## Version 0.1.0
 
 ## Installation
 
-You can install fsapi and its dependencies with npm: `npm install fsapi`.
+You can install fsapi and its dependencies with npm by adding it to your package.json, or running `npm install fsapi`.
 
+    "dependencies": {
+      "fsapi": "0.1.0"
+    }
 
 ## Getting started
 
-Coming soon...
+There is a simple example Express application in the `example` directory. To run the example, you need to add your developer key in place of the `developerKey: 'NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN'`, and then run:
+
+    node example.js
+
+You can test the app in your browser at http://127.0.0.1:3113/
+Ig you want to run it on a different host, then update the `callbackURL: "http://127.0.0.1:"+PORT+"/auth/familysearch/callback"` to match where you are running it.
 
 ### Setup API 
 
 The developerKey listed below can be obtained from [devnet.familysearch.org/](https://devnet.familysearch.org/).
 
-	var FSApi = require('fsapi');
+    var FSApi = require('fsapi');
 
-	var fsapi = new FSApi({
-		developerKey: 'NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN',
-		userAgent: 'My Cool App/1.0'
-	});
+    var fsapi = require('../lib/fsapi')({
+      developerKey: 'NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN',
+      userAgent: "node-fsapi-example-express/0.1.0",
+      referenceHost: "https://sandbox.familysearch.org",
+      callbackURL: "http://127.0.0.1:"+PORT+"/auth/familysearch/callback"
+    });
 
+The required parameters are `developerKey` and `callbackURL`, which must match the URL where your app can be reached.
+
+If you leave referenceHost blank, it will default to https://api.familysearch.org
 
 ### REST API 
 
@@ -46,8 +59,7 @@ The developerKey listed below can be obtained from [devnet.familysearch.org/](ht
 
 ## TODO
 
-- oauth
-- proxy
+- document the proxy
 - search
 - pedigree
 - person read
--- example/example.js
+++ example/example.js
@@ -1,50 +1,71 @@
-var winston = require('winston');
-var logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({ level: 'silly', colorize: true })/*,
-     new (winston.transports.File)({ filename: 'somefile.log' })*/
-  ]
-});
+var PORT = 3113;
+
 var fsapi = require('../lib/fsapi')({
-    developerKey: 'WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK',
-    userAgent: "node-fsapi-example/0.1.0",
-    referenceHost: "https://sandbox.familysearch.org"
+  developerKey: 'NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN',
+  userAgent: "node-fsapi-example-express/0.1.0",
+  referenceHost: "https://sandbox.familysearch.org",
+  callbackURL: "http://127.0.0.1:"+PORT+"/auth/familysearch/callback"
 });
 
-logger.silly('start');
+var express = require('express');
 
-//console.dir(fsapi);
-//console.log("fsapi:");//, fsapi, "FS", fsapi.FS, fsapi.FS.toString());
-//console.dir(fsapi.FS);
-//console.dir(fsapi.FS("session"));
-//logger.silly("fsapi:", fsapi);
-//logger.silly("fsapi:\n", fsapi);
-//console.dir(fsapi);
+var app = express.createServer();
+app.configure(function() {
+  app.use(express.logger());
+  app.use(express.favicon());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({
+    secret: 'something random'
+  }));
+  // fsapi middleware for session support, needs to come after express.session()
+  fsapi.helpExpress(app);
+});
 
-logger.silly("fsapi login:");
-//fsapi.login();
-
-fsapi
-  .validateSession(function (err, data) {
-    logger.silly("verify: " + data);
-    logger.silly("err: " + err);
-  })
-  .tree('' + fsapi.VERSION,
-  function (err, data) {
-    logger.silly("update: " + data);
-    logger.silly("err: " + err);
+app.get('/', function(req, res){
+  if (req.user) {
+    res.send('<html><body><p>Welcome, '+req.user.displayName+'</p><a href="logout">Sign Out of FamilySearch</a><br><hr><p>User:<br><pre>'+JSON.stringify(req.user)+'</pre></p><a href="tree">My Tree</a><br><a href="tree/KWQZ-838">William Savage Tree</a><br><a href="tree/KW31-NJ9">Theras Allred Tree</a><br><a href="search">Search</a></body></html>');
+  } else {
+    res.send('<html><body><a href="auth/familysearch">Sign In to FamilySearch</a><br><hr><p>User:<br><pre>'+JSON.stringify(req.user)+'</pre></p><p>These links will trigger authentication:</p><a href="tree">My Tree</a><br><a href="tree/KWQZ-838">William Savage Tree</a><br><a href="tree/KW31-NJ9">Theras Allred Tree</a><br><a href="search">Search</a></body></html>');
   }
-);
+});
 
-fsapi.search('Theras Orson Allred',
-  function (err, data) {
-    console.log(err, data);
-    console.dir(data);
-  }
-);
+app.get('/tree/:personId?', fsapi.requiresAuthentication, function(req, res){
+  console.log('/tree', req.params.personId);
 
-//fsapi.search('nodejs OR #node', {}, function (err, data) {
-//  logger.silly("search: " + data);
-//  console.dir(data);
-//  logger.silly("err: " + err);
-//});
+  var results= "no data\n";
+  fsapi
+      .tree(req.params.personId || "", {sessionId: req.user.sessionId},
+      function (err, data) {
+        console.log("tree error: ", err);
+        console.log("tree result: ", data);
+        results = JSON.stringify(data, null, 2);
+        res.send('<html><body><p>Welcome, '+req.user.displayName+'</p><a href="logout">Sign Out of FamilySearch</a><br><hr><p>Tree Results:<br><pre>'+results+'</pre></p></body></html>');
+      }
+  );
+});
+
+app.get('/search', fsapi.requiresAuthentication, function(req, res) {
+  console.log('/search');
+  res.send('<html><body><p>Welcome, '+req.user.displayName+'</p><a href="logout">Sign Out of FamilySearch</a><br><hr><br><form action="search" method="post"><input type="text" name="searchName" value="Theras Allred"><input type="submit" value="Search"></form></body></html>');
+});
+
+app.post('/search', fsapi.requiresAuthentication, function(req, res) {
+  console.log('post /search', req.body);
+
+  var results= "no data\n";
+  fsapi.search(req.body.searchName, {sessionId: req.user.sessionId},
+      function (err, data) {
+        console.log("search error:", err);
+        console.log("search result:", data);
+        results = "\nsearch results:\n"+JSON.stringify(data, null, 2);
+        res.send('<html><body><p>Welcome, '+req.user.displayName+'</p><a href="logout">Sign Out of FamilySearch</a><br><hr><p>Results:<br><pre>'+results+'</pre></p></body></html>');
+      }
+  );
+
+});
+
+app.listen(PORT);
+console.log("Express server listening on port %d in %s mode", PORT, app.settings.env);
+
